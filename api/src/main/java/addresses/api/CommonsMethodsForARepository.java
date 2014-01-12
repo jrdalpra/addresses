88a8f386ac6241e36111dd36338bd7c7d8a8b406
jrdalpra <jrdalpra@gmail.com>
2014-01-12 17:17:51 -0200

    submodules

--- api/src/main/java/addresses/api/CommonsMethodsForARepository.java
+++ api/src/main/java/addresses/api/CommonsMethodsForARepository.java
@@ -3,7 +3,8 @@ package addresses.api;
 import java.util.List;
 
 public interface CommonsMethodsForARepository<T, K> {
-	T get(K id);
 
-	List<T> list(int page, int max);
+    T get(K id);
+
+    List<T> list(int page, int max);
 }
